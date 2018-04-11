3c2a2c7d9dbe0071786c26843ba02565e92ed811
Unknown <fpiochaud@gmail.com>
2018-04-11 21:02:51 +1100

    feat(alias.sh): add extrem git command
    source: https://www.synbioz.com/blog/les_aliases_git_de_l_extreme

--- aliases.sh
+++ aliases.sh
@@ -114,4 +114,127 @@ alias master='git checkout master'
 ignore() {
 	echo "$1" >> .gitignore 
 }
+# source https://www.synbioz.com/blog/les_aliases_git_de_l_extreme
+alias state='git fetch --prune ; git fetch --tags ; clear && git branch -vv && git status'
+git-update() {
+  branch=$1
 
+  if [[ -z $branch ]]
+  then
+    branch=`git symbolic-ref HEAD | cut -d "/" -f 3-`
+  else
+    git checkout $branch
+  fi
+
+  git pull --rebase origin $branch
+}
+
+alias update='git-update'
+
+git-updates() {
+  git stash && git-update "$@" && git stash pop
+}
+
+alias updates='git-updates'
+
+alias tagz='git tag -l | sort -V'
+alias nxtrlz='tagz | tail -n 1 && git log --oneline `tagz | tail -n 1`..HEAD'
+
+git-merged() {
+  sourceBranch=`git symbolic-ref HEAD | cut -d "/" -f 3-`
+  targetBranch=$1
+
+  if [[ -z $targetBranch ]]
+  then
+    targetBranch='master'
+  fi
+
+  git-update $targetBranch && git branch -d $sourceBranch
+}
+
+alias merged='git-merged'
+
+git-mergeds() {
+  git stash && git-merged "$@" && git stash pop
+}
+
+alias mergeds='git-mergeds'
+
+git-lstcmt() {
+  git log --oneline $1 | grep -v fixup | head -n 1 | cut -d " " -f 1
+}
+
+alias lstcmt='git-lstcmt'
+
+git-logz() {
+  nb=$2
+
+  if [[ -z $nb ]]
+  then
+      nb=10
+  fi
+
+  git log --oneline $1 | head -n $nb
+}
+
+alias logz='git-logz'
+
+git-rebase() {
+  sourceBranch=`git symbolic-ref HEAD | cut -d "/" -f 3-`
+  targetBranch=$1
+
+  if [[ -z $targetBranch ]]
+  then
+    targetBranch='master'
+  fi
+
+  git-update $targetBranch && git checkout $sourceBranch && git rebase $targetBranch
+}
+
+alias rebase='git-rebase'
+
+alias goon='git add . && git rebase --continue || git rebase --skip'
+
+git-rebases() {
+  git stash && git-rebase "$@" && git stash pop
+}
+
+alias rebases='git-rebases'
+
+git-squash() {
+  ref=$1
+
+  if [[ -z $ref ]]
+  then
+    ref=`git log --oneline | grep -v fixup | head -n 2 | tail -n 1 | cut -d " " -f 1`
+  fi
+
+  git rebase -i --autosquash $ref
+}
+
+alias squash='git-squash'
+
+git-fixup() {
+  ref=$1
+
+  if [[ -z $ref ]]
+  then
+    ref=`lstcmt`
+  fi
+
+  git commit --fixup $ref
+}
+
+alias fixup='git-fixup'
+
+alias fixupa='adal && fixup'
+
+alias adal='git add --all .'
+
+alias amend='git commit --amend --no-edit'
+alias amenda='adal && amend'
+
+alias yolo='git push origin HEAD --force-with-lease'
+
+alias yoloc='amend && yolo'
+alias yoloca='adal && yoloc'
